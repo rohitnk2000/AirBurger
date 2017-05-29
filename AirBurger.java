fefd0b6b773269cfa2b6c68a6383c04a5168a098
Isa Dash <isadash21@gmail.com>
2017-05-29 16:55:00 -0700

    Update AirBurger.java

--- AirBurger.java
+++ AirBurger.java
@@ -1,23 +1,57 @@
+package src;
+import java.util.Scanner;
+
 import javax.swing.*;
+
 public class AirBurger extends JFrame
 {
-
-    public AirBurger () throws Exception
+    static double cashEarned;
+    static Order o;
+    public AirBurger() throws Exception
     {
-	this.getContentPane ().add (new Order ());
+        o = new Order();
+        this.getContentPane().add(o);
+        
 
-	this.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-	this.setSize (800, 700);
-	this.setLocationRelativeTo (null);
-	this.setTitle ("AirBurger");
-	this.setResizable (false);
-	this.setVisible (true);
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 700);
+        this.setLocationRelativeTo(null);
+        this.setTitle("AirBurger");
+        this.setResizable(false);
+        this.setVisible(true);
     }
 
-
-    public static void main (String[] args) throws Exception
+    public static void main(String[] args) throws Exception
     {
-	new AirBurger ();
+        System.out.println( "Username:" );
+        Scanner scan = new Scanner( System.in );
+        String username = scan.nextLine();
+        FileReaderAndWriter frw = new FileReaderAndWriter(username);
+
+        frw.read(
+            "/Users/appx/Dropbox/workspaceAPCS/AirBurger1/src/Users.txt" );
+        frw.login();
+        try
+        {
+            new AirBurger();
+            //cashEarned = o.cash;
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        while(o.gameInProgress){
+            System.out.print("");
+            
+        }
+        //System.out.println("here");
+        frw.changeScore( o.cash );
+
+        frw.write();
+        
+        
+        
     }
 }
